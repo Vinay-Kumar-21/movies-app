9e0c8fb655beff96a0ca1e9d184e3a3c633858d3
Vinay Kumar <vinaykumarraju21@gmail.com>
2024-02-15 07:59:28 +0530

    created movies-app

---
+++ src/Components/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import NavBar from "./NavBar";
+import Banner from "./Banner";
+import TrendingMovies from "./TrendingMovies";
+import Pagination from "./Pagination";
+
+
+function Home() {
+    return (
+        <>
+            <div>Home</div>
+            <NavBar></NavBar>
+            <Banner></Banner>
+            <TrendingMovies></TrendingMovies>
+            <Pagination></Pagination>
+        </>
+
+    )
+}
+
+export default Home;
