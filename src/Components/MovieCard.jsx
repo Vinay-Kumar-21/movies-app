5a685dde39acff67c7bf67f1fbea92889e181aa9
Vinay Kumar <vinaykumarraju21@gmail.com>
2024-02-22 22:33:35 +0530

    emoji,local storage,add & remove of watchlist

--- src/Components/MovieCard.jsx
+++ src/Components/MovieCard.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 
-function MovieCard({ title, poster_path }) {
+function MovieCard({ title, poster_path, id,
+    watchList,
+    handleAddToWatchList,
+    handleRemoveFromWatchList }) {
+
     return (
-        <div className="flex justify-center items-end h-80 w-56 rounded-lg overflow-hidden bg-cover bg-no-repeat"
+        <div className="flex flex-col justify-between hover:scale-[0.9] duration-200 justify-center items-end h-80 w-56 rounded-lg overflow-hidden bg-cover bg-no-repeat"
             style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original${poster_path})` }}
         >
+            {watchList.includes(id) ?
+                <div onClick={() => handleRemoveFromWatchList(id)} className='text-2xl m-2 py-0.5 rounded cursor-pointer bg-slate-950/50'>&#10060;</div> :
+                <div onClick={() => handleAddToWatchList(id)} className='text-2xl m-2 py-0.5 rounded cursor-pointer bg-slate-950/50'>&#128525;</div>
+            }
+
             <div className="text-white py-0.5 bg-slate-950/50 w-full text-center">
                 {title}
             </div>
--- src/Components/TrendingMovies.jsx
+++ src/Components/TrendingMovies.jsx
@@ -6,6 +6,21 @@ import axios from "axios";
 function TrendingMovies() {
     const [movies, setMovies] = useState([]);
     const [pageNo, setPageNo] = useState(1);
+    const [watchList, setWatchList] = useState([]);
+
+    const handleAddToWatchList = (id) => {
+        const newWatchList = [...watchList];
+        newWatchList.push(id);
+        localStorage.setItem('watchList', JSON.stringify(newWatchList));
+        setWatchList(newWatchList);
+    }
+    const handleRemoveFromWatchList = (id) => {
+        const newWatchList = watchList.filter((movieId) => {
+            return id !== movieId;
+        })
+        localStorage.setItem('watchList', JSON.stringify(newWatchList));
+        setWatchList(newWatchList);
+    }
 
     const handleNext = function () {
         setPageNo(pageNo + 1);
@@ -17,7 +32,10 @@ function TrendingMovies() {
         }
 
     }
-
+    useEffect(() => {
+        let watchListFromLocalStorage = JSON.parse(localStorage.getItem('watchList'));
+        setWatchList(watchListFromLocalStorage);
+    }, [])
     useEffect(() => {
         axios.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=b96471c489fa528e5b4d6882d93e4312&page=${pageNo}`)
             .then(function (response) {
@@ -37,14 +55,19 @@ function TrendingMovies() {
                     movies.map((movieObj) => {
                         return <MovieCard
                             key={movieObj.id}
+                            id={movieObj.id}
                             title={movieObj.title}
-                            poster_path={movieObj.poster_path} />
+                            poster_path={movieObj.poster_path}
+                            watchList={watchList}
+                            handleAddToWatchList={handleAddToWatchList}
+                            handleRemoveFromWatchList={handleRemoveFromWatchList} />
                     })
                 }
             </div>
             <Pagination pageNo={pageNo}
                 handleNext={handleNext}
-                handlePrev={handlePrev} />
+                handlePrev={handlePrev}
+            />
         </>
 
     )
